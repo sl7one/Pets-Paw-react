9b197e95fb53622d0a6b04f5e525477d5a32452d
Slava <110696118+sl7one@users.noreply.github.com>
2023-09-01 14:41:50 +0300

    upd

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,27 +1,32 @@
-import React from "react";
-import "./search-bar.scss";
-import Icon from "../Icon/Icon";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './search-bar.scss';
+import Icon from '../Icon/Icon';
+import { Link } from 'react-router-dom';
 
 interface IProps {
-  value: string;
-  onChange: (value: string) => void;
+   value: string;
+   onChange: (value: string) => void;
 }
 
-export default function SearchBar({ value = "", onChange }: IProps) {
-  return (
-    <form className="search-bar">
-      <input
-        type="text"
-        value={value}
-        placeholder="Search for breeds by name"
-        onChange={({ target: { value } }) =>
-          onChange(value.toLowerCase())
-        }
-      />
-      <Link className="submit" to={`/search?q=${value}`}>
-        <Icon name="icon-search" height={20} width={20}></Icon>
-      </Link>
-    </form>
-  );
+export default function SearchBar({ value = '', onChange }: IProps) {
+   return (
+      <form className="search-bar">
+         <input
+            type="text"
+            value={value}
+            placeholder="Search for breeds by name"
+            onChange={({ target: { value } }) => onChange(value.toLowerCase())}
+         />
+         <Link
+            className="submit"
+            to={`/search?q=${value}`}
+         >
+            <Icon
+               name="icon-search"
+               height={20}
+               width={20}
+            ></Icon>
+         </Link>
+      </form>
+   );
 }
